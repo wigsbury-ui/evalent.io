b34dcc1190cadaf08426908ab856ef4075580d5c
wigsbury-ui <wigsbury@gmail.com>
2025-11-06 12:46:40 +0400

    Delete api directory

--- api/ping.ts
+++
@@ -1,1 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'; export default (req:VercelRequest,res:VercelResponse)=>res.status(200).json({ok:true,route:'/api/ping',time:new Date().toISOString()});
