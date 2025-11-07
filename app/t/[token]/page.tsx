a85617c46cc2204530aa89ee3512766746971a06
wigsbury-ui <wigsbury@gmail.com>
2025-11-07 08:49:27 +0400

    Update page.tsx

--- app/t/[token]/page.tsx
+++ app/t/[token]/page.tsx
@@ -1,7 +1,7 @@
-// app/dev/t/[token]/page.tsx
+// app/t/[token]/page.tsx
 import { redirect } from "next/navigation";
 
-export default function DevTokenPage({ params }: { params: { token: string } }) {
-  // Normalize any /dev/t/[token] link to the actual runner:
+export default function RootTokenPage({ params }: { params: { token: string } }) {
+  // Also catch old /t/[token] links:
   redirect(`/dev/test?token=${params.token}`);
 }
