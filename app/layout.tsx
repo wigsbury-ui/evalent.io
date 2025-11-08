195798543098bc699a44ad0c26fe9a3fed10487c
wigsbury-ui <wigsbury@gmail.com>
2025-11-08 11:01:38 +0400

    Delete app directory

--- app/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ fontFamily: "Georgia, serif", padding: 24 }}>{children}</body>
-    </html>
-  );
-}
