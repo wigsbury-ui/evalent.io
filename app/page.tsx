3ce72a5b3e2224435c644ff6cd2af2cb58cbdf70
wigsbury-ui <wigsbury@gmail.com>
2025-11-08 10:02:19 +0400

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1 style={{ fontSize: 64, lineHeight: 1.05 }}>Evalent</h1>
-      <p>Root page. Open a test session via your helper page to get a token, then visit <code>/t/&lt;token&gt;</code>.</p>
-    </main>
-  );
-}
