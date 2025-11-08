ad77e93518b2cb711597d176c6f19efae3b6041e
wigsbury-ui <wigsbury@gmail.com>
2025-11-08 09:38:36 +0400

    Delete lib directory

--- lib/db.ts
+++
@@ -1,9 +0,0 @@
-// lib/db.ts
-import { createClient } from '@supabase/supabase-js';
-
-const url  = process.env.SUPABASE_URL!;
-const key  = process.env.SUPABASE_SERVICE_ROLE_KEY!; // use a service key on the server
-
-export const supa = createClient(url, key, {
-  auth: { persistSession: false },
-});
