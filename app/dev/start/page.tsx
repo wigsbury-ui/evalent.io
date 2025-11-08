d934d3443b26845cbd4913af16794d02f99e22e8
wigsbury-ui <wigsbury@gmail.com>
2025-11-08 09:38:17 +0400

    Delete app directory

--- app/dev/start/page.tsx
+++
@@ -1,3 +0,0 @@
-// app/dev/start/page.tsx
-export { default } from "../../start/page";
-export const dynamic = "force-dynamic";
--- app/dev/t/[token]/page.tsx
+++
@@ -1,2 +0,0 @@
-// app/dev/t/[token]/page.tsx
-export { default } from "../../../t/[token]/page";
