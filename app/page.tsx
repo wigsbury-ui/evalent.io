3be751e9fccb82111ba8936550fc65a397abe2a7
wigsbury-ui <wigsbury@gmail.com>
2025-11-08 11:23:20 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,3 +2,5 @@ import { redirect } from "next/navigation";
 export default function Home() {
   redirect("/start");
 }
+import { redirect } from "next/navigation";
+export default function Home() { redirect("/start"); }
