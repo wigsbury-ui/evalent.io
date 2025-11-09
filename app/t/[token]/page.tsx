9055457f264356a913685a9e7ee71c78e09daa7d
wigsbury-ui <wigsbury@gmail.com>
2025-11-09 12:30:46 +0400

    Update page.tsx

--- app/t/[token]/page.tsx
+++ app/t/[token]/page.tsx
@@ -1,14 +1,13 @@
 // app/t/[token]/page.tsx
-import RunnerClient from "./RunnerClient";
+import RunnerClient from './RunnerClient';
 
-export default function TokenPage({ params }: { params: { token: string } }) {
+export default function Page({ params }: { params: { token: string } }) {
   return (
     <main style={{ padding: 24 }}>
       <h1 style={{ fontSize: 64, lineHeight: 1.05 }}>Evalent Test Runner</h1>
       <p style={{ fontSize: 22, marginTop: 8 }}>
         Token: <code>{params.token}</code>
       </p>
-
       <RunnerClient token={params.token} />
     </main>
   );
