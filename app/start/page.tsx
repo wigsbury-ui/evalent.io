799df886d0c75765b7ad61f0ff7cf1f10b65eece
wigsbury-ui <wigsbury@gmail.com>
2025-11-13 07:55:21 +0400

    Update page.tsx

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -5,9 +5,5 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default function Page() {
-  return (
-    <div className="max-w-5xl mx-auto pt-10">
-      <StartFormClient />
-    </div>
-  );
+  return <StartFormClient />;
 }
