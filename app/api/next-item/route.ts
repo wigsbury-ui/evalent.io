6d4f72a0ec9bcc826b7c797f8ab832503ebd1bab
wigsbury-ui <wigsbury@gmail.com>
2025-11-14 17:23:26 +0400

    Update route.ts

--- app/api/next-item/route.ts
+++ app/api/next-item/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import supabaseAdmin from '@/lib/supabaseAdmin';
+import { supabaseAdmin } from '@/lib/supabaseAdmin';
 
 const QUESTIONS_PER_SESSION = 8;
 
